d969d961749cd696120ca97ed3b5f1b5e493d66e
ottelo9 <33122175+ottelo9@users.noreply.github.com>
2026-02-27 12:00:44 +0100

    Optimization of V6 and V7 code and add V8, add EEPROM GUI
    - add the reworked, optimized V6 V7 and new V8 BMS code from bluespresso. He added his work as a zip [here](https://github.com/tinfever/FW-Dyson-BMS/issues/76)
    - add the GUI tool from https://github.com/CritecRO/FU-Dyson-BMS/tree/main/EEPROM-parsing-tool/ to this repo, its from CritecRO, thx for his work!
    
    TODO:
    Update readme.

---
+++ firmware/V6_V7/main.h
@@ -0,0 +1,127 @@
+/*
+* FU-Dyson-BMS	-	(unofficial) Firmware Upgrade for Dyson BMS - V6/V7 Vacuums
+* Copyright (C) 2022 tinfever
+* 
+* This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+* 
+* The author can be contacted at tinfever6@(insert-everyone's-favorite-google-email-domain).com
+* 
+* NOTE: As an addendum to the GNU General Public License, any hardware using code or information from this project must also make publicly available complete electrical schematics and a bill of materials for such hardware.
+*/
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#include "mcc_generated_files/adc.h"
+
+enum {
+    INIT = 0,
+    SLEEP,
+    IDLE,
+    CHARGING,
+    CHARGING_WAIT,
+    CELL_BALANCE,
+    OUTPUT_EN,
+    ERROR,
+} state;
+
+typedef enum{
+    NONE = 0,       //0b00
+    TRIGGER = 1,    //0b01
+    CHARGER = 2     //0b10
+} detect_t;
+
+detect_t detect = 0;
+//#define DETECT_HISTORY_SIZE 4
+//detect_t detect_history[DETECT_HISTORY_SIZE] = 0;
+//uint8_t oldest_detect_index = 0;
+
+uint8_t detect_history = 0; //Bits 0-1 = position 0; Bits 2-3 = position 1; Bits 4-5 = position 2; Bits 6-7 = position 3
+
+uint16_t adc_chrg_trig_detect_voltage_history[5];
+
+uint8_t pack_charge_wait_repeats_counter = 0; // Counter for WaitCharge -> Charge repetations
+
+uint8_t enable_slow_charge = 0; // for slow charging if mincell voltage below 3000mV but no Error 
+
+typedef enum {
+    SV09 = 0,
+    SV11 = 1,
+    NUM_OF_MODELS,
+} modelnum_t;
+modelnum_t modelnum;
+
+int16_t isl_int_temp;
+int16_t isl_int_temp_max = 0;
+uint8_t thermistor_temp;
+uint8_t thermistor_temp_max = 0;
+bool charge_complete_flag = false;
+bool full_discharge_flag = false;
+uint16_t discharge_current_mA = 0;
+uint16_t discharge_current_mA_last_trigger = 0;  //last discharge current while on trigger to calculate internal resistance
+uint16_t discharge_current_mA_last_trigger_eeprom = 0; //record for eeprom discharge current for highest internal resistence
+uint16_t mincell_voltage_mV_last_trigger = 0xFFFF;  //last discharge mincell voltage while on trigger to calculate internal resistance
+uint8_t minimum_cell_last_trigger = 0; // record which wass the cell with the miniumum voltage when last output was enabled
+uint32_t mincell_internal_resistence_last_charge_uOhms = 0;  //will be calculated from difference in voltage mincell between when trigger last pulled to charge start
+uint32_t mincell_internal_resistence_max = 0; //record highest recorded internal resistence
+uint16_t packdelta_end_of_charging_wait_mV = 0;
+uint16_t mincell_voltage_mV_last_trigger_eeprom = 0;
+uint16_t mincell_voltage_mV_last_trigger_recover_eeprom = 0;
+uint8_t I2C_error_counter = 0;
+int8_t cell_offset_voltage_1 = 0;
+int8_t cell_offset_voltage_2 = 0;
+int8_t cell_offset_voltage_3 = 0;
+int8_t cell_offset_voltage_4 = 0;
+int8_t cell_offset_voltage_5 = 0;
+int8_t cell_offset_voltage_6 = 0;
+
+    
+
+
+typedef struct {
+    uint16_t value;
+    bool enable;
+} counter_t;
+
+typedef struct {
+    uint32_t value;
+    bool enable;
+} big_counter_t;
+
+counter_t charge_wait_counter = {0,0};
+counter_t sleep_timeout_counter = {0,0};
+counter_t nonblocking_wait_counter = {0,0};
+counter_t error_timeout_wait_counter = {0,0};
+big_counter_t charge_duration_counter = {0,0};
+counter_t LED_code_cycle_counter = {0,0};
+big_counter_t total_runtime_counter = {0,0};
+big_counter_t onetime_runtime_counter = {0,0};
+
+detect_t GetDetectHistory(uint8_t position);
+bool CheckStateInDetectHistory(detect_t detect_val);
+
+uint16_t readADCmV(adc_channel_t channel);
+void Write32BitUintVariableToEEPROM(uint8_t starting_addr, uint32_t variable_to_write);
+uint32_t Read32BitUintVariableFromEEPROM(uint8_t starting_addr);
+void Write16BitUintVariableToEEPROM(uint8_t starting_addr, uint16_t variable_to_write);
+uint16_t Read16BitUintVariableFromEEPROM(uint8_t starting_addr);
+void ClearI2CBus(void);
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif	/* MAIN_H */
+
---
+++ firmware/V8/LED.c
@@ -0,0 +1,142 @@
+/*
+* FU-Dyson-BMS	-	(unofficial) Firmware Upgrade for Dyson BMS - V6/V7 Vacuums
+* Copyright (C) 2022 tinfever
+* 
+* This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+* 
+* The author can be contacted at tinfever6@(insert-everyone's-favorite-google-email-domain).com
+* 
+* NOTE: As an addendum to the GNU General Public License, any hardware using code or information from this project must also make publicly available complete electrical schematics and a bill of materials for such hardware.
+*/
+
+#include "main.h"
+#include "LED.h"
+#include "mcc_generated_files/epwm1.h"
+#include "config.h"
+#include "isl94208.h"
+
+// Example: ledBlinkpattern (3, 0b110, 250, 250, 750, 500, 32);
+// This would give 3 yellow (0b110 RGB) blinks, with 250ms ontime and 250ms off time
+// There would be a 750ms blank time before and 500ms blank time after the three blinks
+// There is a PWM fade slope of positive 32, so each blink will fade in at a rate of +32 to the PWM level per loop iteration
+void ledBlinkpattern (uint8_t num_blinks, uint8_t led_color_rbbb,
+                    uint16_t blink_on_time_ms, uint16_t blink_off_time_ms,
+                    uint16_t starting_blank_time_ms, uint16_t ending_blank_time_ms){       
+    uint16_t timer_ms = nonblocking_wait_counter.value*32;
+    
+    static uint8_t max_steps = 0; 
+    static uint8_t step = 0;
+    static uint16_t next_step_time = 0;
+    
+    /*Example step schedule for num_blinks = 3, blink_interval_ms = 500, starting_blank_time_ms = 1000, ending_blank_time_ms = 1000
+    *Step #     End time    LED ON
+    * 0          1000,      0     //starting blank interval
+    * 1          1500,      1     //blink one
+    * 2          2000,      0
+    * 3          2500,      1     //blink two
+    * 4          3000,      0
+    * 5          3500,      1     //blink three
+    * 6          4000,      0
+    * 7          5000,      0     //ending blank interval
+     */
+        
+    //Initialize
+    if (!nonblocking_wait_counter.enable){
+        Set_LED_RBBB(0b0000);     //Turn off LED
+        nonblocking_wait_counter.value = 0;
+        nonblocking_wait_counter.enable = 1;
+        max_steps = (2*num_blinks+2)-1;     //Subtract one so it is zero indexed
+        step = 0;
+        next_step_time = starting_blank_time_ms;
+        if (LED_code_cycle_counter.enable){
+            LED_code_cycle_counter.value++;
+        }
+    }
+    
+    if (step == 0 && timer_ms > next_step_time){                        //starting blank time
+        step++;
+        if (num_blinks != 0){   // Leave LED off if num_blinks is zero. Next step will be cycle complete.
+            Set_LED_RBBB(led_color_rbbb);     //Turn on LED
+        }
+        next_step_time += blink_on_time_ms;
+    }
+    else if (step == max_steps-1 && timer_ms > next_step_time){         //ending blank time
+        step++;
+        Set_LED_RBBB(0b0000);     //Turn off LED
+        next_step_time += ending_blank_time_ms;
+    }
+    else if (step == max_steps && timer_ms > next_step_time){           //cycle complete
+        Set_LED_RBBB(0b0000);     //Turn off LED
+        nonblocking_wait_counter.enable = 0;
+        nonblocking_wait_counter.value = 0;
+    }
+    else if (step % 2 != 0 && timer_ms > next_step_time){        //Step number is odd
+        step++;
+        Set_LED_RBBB(0b0000);     //Turn off LED
+        next_step_time += blink_off_time_ms;
+    }
+    else if (step % 2 == 0 && timer_ms > next_step_time){       //Step number is even
+        step++;
+        Set_LED_RBBB(led_color_rbbb);     //Turn on LED
+        next_step_time += blink_on_time_ms;
+    }
+       
+}
+
+void resetLEDBlinkPattern (void){
+    Set_LED_RBBB(0b0000);     //Turn off LED
+    nonblocking_wait_counter.enable = false;
+    nonblocking_wait_counter.value = 0;
+    LED_code_cycle_counter.enable = false;
+    LED_code_cycle_counter.value = 0;
+    
+}
+// Accepts binary input 0b000. Bit 2 = Red Enable. Bit 1 = Green Enable. Bit 0 = Red Enable. R.G.B.
+// PWM_val sets PWM brightness level 0-1023
+void Set_LED_RBBB(uint8_t RBBB_en){  
+        
+    if (RBBB_en & 0b1000){
+        LATA = (LATA & 0b01111111); //Turns on red LED
+    }
+    else{
+        LATA = (LATA | 0b10000000); //Turns off red LED
+    }
+    
+    if (RBBB_en & 0b0001){
+        LATA = (LATA & 0b10111111); //Turns on blue LED 1
+    }
+    else{
+        LATA = (LATA | 0b01000000); //Turns off blue LED 1
+    }
+    
+    if (RBBB_en & 0b0010){
+        LATB = (LATB & 0b11111110); //Turns on blue LED 2
+    }
+    else{
+        LATB = (LATB | 0b00000001); //Turns off blue LED 2
+    }
+    
+    if (RBBB_en & 0b0100){
+        LATB = (LATB & 0b11110111); //Turns on blue LED 3
+    }
+    else{
+        LATB = (LATB | 0b00001000); //Turns off blue LED 3
+    }
+}
+
+bool cellDeltaLEDIndicator (void){
+    uint8_t num_yellow_blinks = (uint8_t) ( (packdelta_end_of_charging_wait_mV+25) / 50 );      //One blink per 50mV min-max cell delta. Adding 25 to do traditional rounding
+    LED_code_cycle_counter.enable = true;
+    ledBlinkpattern (num_yellow_blinks, 0b1001, 250, 250, 750, 500);
+    if (LED_code_cycle_counter.value > 1){
+        resetLEDBlinkPattern();
+        return true;
+    }
+    else {
+        return false;
+    }
+}
